0c0be81349c24d260669ba462be16c4237e75cdf
alivka-oko <alina.okotnikova2002@gmail.com>
2024-05-22 09:30:11 +0300

    легкая поправка

--- wp-content/themes/simple-bootstrap-theme/home.php
+++ wp-content/themes/simple-bootstrap-theme/home.php
@@ -34,32 +34,32 @@ get_header();
                     <h2 class="heading-m text-up">
                         Лучшие предложения
                     </h2>
-                    <a href="<?php echo esc_url(get_permalink(wc_get_page_id('shop'))); ?>" class="link-accent">Все предложения</a>
+                    <a href="<?php echo esc_url(get_permalink(wc_get_page_id('shop'))); ?>" class="link-accent">Все
+                        предложения</a>
                 </div>
                 <div class="cards">
                     <?php
                     $args = array(
-                        'post_type'           => 'product',
-                        'posts_per_page'      => $products,
-                        'orderby'             => $orderby,
+                        'post_type' => 'product',
                         'posts_per_page' => 8,
-                        'order'               => $order == 'asc' ? 'asc' : 'desc',
-                        'post__in'            => wc_get_featured_product_ids(),
-                        'tax_query'      => array(
+                        'order' => $order == 'asc' ? 'asc' : 'desc',
+                        'post__in' => wc_get_featured_product_ids(),
+                        'tax_query' => array(
                             array(
                                 'taxonomy' => 'product_cat', // Название таксономии для категорий товаров
-                                'field'    => 'slug', // Используем slug для поиска категории
-                                'terms'    => 'spaces', // Slug категории, из которой нужно получить товары
+                                'field' => 'slug', // Используем slug для поиска категории
+                                'terms' => 'spaces', // Slug категории, из которой нужно получить товары
                             ),
                         ),
                     );
                     $query = new WP_Query($args);
-                    if ($query->have_posts()) :
+                    if ($query->have_posts()):
                         $i = 1;
-                        while ($query->have_posts()) : $query->the_post();
+                        while ($query->have_posts()):
+                            $query->the_post();
                             $product = wc_get_product(get_the_ID());
 
-                            if ($product && $product->is_visible()) :
+                            if ($product && $product->is_visible()):
                                 $address = CFS()->get('address');
                                 $area = CFS()->get('area');
                                 $floor = CFS()->get('floor');
@@ -68,19 +68,23 @@ get_header();
 
                                 $attachment_ids = $product->get_gallery_image_ids();
                                 $post_thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), array(500, 500));
-                    ?>
+                                ?>
                                 <article class="card product">
                                     <div class="swiper-container products">
                                         <div class="swiper swiper-<?= $i ?>">
                                             <div class="swiper-wrapper">
-                                                <?php if ($post_thumbnail_url) : ?>
+                                                <?php if ($post_thumbnail_url): ?>
                                                     <div class="swiper-slide">
-                                                        <div class="swiper-img"><a href="<?php echo the_permalink() ?>"><img src="<?= esc_url($post_thumbnail_url); ?>" alt="<?php echo esc_attr(get_the_title()); ?>"></a></div>
+                                                        <div class="swiper-img"><a href="<?php echo the_permalink() ?>"><img
+                                                                    src="<?= esc_url($post_thumbnail_url); ?>"
+                                                                    alt="<?php echo esc_attr(get_the_title()); ?>"></a></div>
                                                     </div>
                                                 <?php endif; ?>
-                                                <?php foreach ($attachment_ids as $attachment_id) : ?>
+                                                <?php foreach ($attachment_ids as $attachment_id): ?>
                                                     <div class="swiper-slide">
-                                                        <div class="swiper-img"><a href="<?php echo the_permalink() ?>"><img src="<?= esc_url(wp_get_attachment_url($attachment_id)) ?>" alt=""></a></div>
+                                                        <div class="swiper-img"><a href="<?php echo the_permalink() ?>"><img
+                                                                    src="<?= esc_url(wp_get_attachment_url($attachment_id)) ?>"
+                                                                    alt=""></a></div>
                                                     </div>
                                                 <?php endforeach ?>
                                             </div>
@@ -92,28 +96,32 @@ get_header();
                                         <h3 class="card-title text-title"><?php the_title(); ?></h3>
                                         <div class="card-address text-value"><?php echo $address ?></div>
                                         <div class="card-description">
-                                            <?php if ($price != 0) : ?>
+                                            <?php if ($price != 0): ?>
                                                 <div class="attribute">
                                                     <span class="text-attribute">Стоимость в месяц</span>
-                                                    <span class="text-value"><?php echo number_format((float)$price, 0, '.', ' '); ?> ₽</span>
+                                                    <span class="text-value"><?php echo number_format((float) $price, 0, '.', ' '); ?>
+                                                        ₽</span>
                                                 </div>
                                             <?php endif ?>
-                                            <?php if ($area and $price) :
+                                            <?php if ($area and $price):
                                                 $price_month_area = $price / $area; ?>
                                                 <div class="attribute">
                                                     <span class="text-attribute">Стоимость за м2/мес.</span>
-                                                    <span class="text-value"><?php echo number_format((float)$price_month_area, 2, '.', ' '); ?> ₽</span>
+                                                    <span
+                                                        class="text-value"><?php echo number_format((float) $price_month_area, 2, '.', ' '); ?>
+                                                        ₽</span>
                                                 </div>
                                             <?php endif ?>
 
-                                            <?php if ($area) : ?>
+                                            <?php if ($area): ?>
                                                 <div class="attribute">
                                                     <span class="text-attribute">Площадь</span>
-                                                    <span class="text-value"><?php echo number_format((float)$area, 0, '.', ' '); ?> м2</span>
+                                                    <span class="text-value"><?php echo number_format((float) $area, 0, '.', ' '); ?>
+                                                        м2</span>
                                                 </div>
                                             <?php endif ?>
 
-                                            <?php if ($floor) : ?>
+                                            <?php if ($floor): ?>
                                                 <div class="attribute">
                                                     <span class="text-attribute">Этаж</span>
                                                     <span class="text-value"><?php echo $floor ?> этаж</span>
@@ -128,14 +136,14 @@ get_header();
                                                 <button>
                                                     <img src="<?= $phoneico ?>" alt="call-ico">
                                                 </button>
-                                            <?php
+                                                <?php
                                             }
                                             ?>
                                         </div>
                                     </div>
                                 </article>
 
-                    <?php
+                                <?php
                             endif;
                             $i++;
                         endwhile;
@@ -151,7 +159,8 @@ get_header();
     <section class="section banner-ad">
         <div class="container">
             <div class="content">
-                <a class="advertisement" href="#" style="background:url('http://renty/wp-content/uploads/2024/05/advertisement.png')"></a>
+                <a class="advertisement" href="#"
+                    style="background:url('../wp-content/uploads/2024/05/advertisement.png')"></a>
                 <div class="banner-ad__text-block">
                     <div class="left-side">
                         <h3 class="heading-m">Бесплатный подбор офиса и экскурсии по объектам!</h3>
@@ -162,7 +171,7 @@ get_header();
                         </div>
                     </div>
                     <div class="right-side">
-                        <img src="http://renty/wp-content/uploads/2024/05/Frame-18136.png" alt="excursion">
+                        <img src="../wp-content/uploads/2024/05/Frame-18136.png" alt="excursion">
                     </div>
                 </div>
             </div>
@@ -188,41 +197,51 @@ get_header();
                         <div class="swiper-wrapper">
                             <div class="swiper-slide">
                                 <a href="#" class="article card">
-                                    <img src="http://renty/wp-content/uploads/2024/04/road-sport-vintage-wheel-retro-old-scaled.jpg" alt="">
+                                    <img src="../wp-content/uploads/2024/04/road-sport-vintage-wheel-retro-old-scaled.jpg"
+                                        alt="">
                                     <h5 class="text-sub">Финансы</h5>
-                                    <p class="text">Как изменятся цены на московское жилье к началу 2023 года. Как можно к этому подготовиться?</p>
+                                    <p class="text">Как изменятся цены на московское жилье к началу 2023 года. Как можно
+                                        к этому подготовиться?</p>
                                     <span class="text-date">22.01.2022</span>
                                 </a>
                             </div>
                             <div class="swiper-slide">
                                 <a href="#" class="article card">
-                                    <img src="http://renty/wp-content/uploads/2024/04/road-sport-vintage-wheel-retro-old-scaled.jpg" alt="">
+                                    <img src="../wp-content/uploads/2024/04/road-sport-vintage-wheel-retro-old-scaled.jpg"
+                                        alt="">
                                     <h5 class="text-sub">Финансы</h5>
-                                    <p class="text">Как изменятся цены на московское жилье к началу 2023 года. Как можно к этому подготовиться?</p>
+                                    <p class="text">Как изменятся цены на московское жилье к началу 2023 года. Как можно
+                                        к этому подготовиться?</p>
                                     <span class="text-date">22.01.2022</span>
                                 </a>
                             </div>
                             <div class="swiper-slide">
                                 <a href="#" class="article card">
-                                    <img src="http://renty/wp-content/uploads/2024/04/road-sport-vintage-wheel-retro-old-scaled.jpg" alt="">
+                                    <img src="../wp-content/uploads/2024/04/road-sport-vintage-wheel-retro-old-scaled.jpg"
+                                        alt="">
                                     <h5 class="text-sub">Финансы</h5>
-                                    <p class="text">Как изменятся цены на московское жилье к началу 2023 года. Как можно к этому подготовиться?</p>
+                                    <p class="text">Как изменятся цены на московское жилье к началу 2023 года. Как можно
+                                        к этому подготовиться?</p>
                                     <span class="text-date">22.01.2022</span>
                                 </a>
                             </div>
                             <div class="swiper-slide">
                                 <a href="#" class="article card">
-                                    <img src="http://renty/wp-content/uploads/2024/04/road-sport-vintage-wheel-retro-old-scaled.jpg" alt="">
+                                    <img src="../wp-content/uploads/2024/04/road-sport-vintage-wheel-retro-old-scaled.jpg"
+                                        alt="">
                                     <h5 class="text-sub">Финансы</h5>
-                                    <p class="text">Как изменятся цены на московское жилье к началу 2023 года. Как можно к этому подготовиться?</p>
+                                    <p class="text">Как изменятся цены на московское жилье к началу 2023 года. Как можно
+                                        к этому подготовиться?</p>
                                     <span class="text-date">22.01.2022</span>
                                 </a>
                             </div>
                             <div class="swiper-slide">
                                 <a href="#" class="article card">
-                                    <img src="http://renty/wp-content/uploads/2024/04/road-sport-vintage-wheel-retro-old-scaled.jpg" alt="">
+                                    <img src="../wp-content/uploads/2024/04/road-sport-vintage-wheel-retro-old-scaled.jpg"
+                                        alt="">
                                     <h5 class="text-sub">Финансы</h5>
-                                    <p class="text">Как изменятся цены на московское жилье к началу 2023 года. Как можно к этому подготовиться?</p>
+                                    <p class="text">Как изменятся цены на московское жилье к началу 2023 года. Как можно
+                                        к этому подготовиться?</p>
                                     <span class="text-date">22.01.2022</span>
                                 </a>
                             </div>
